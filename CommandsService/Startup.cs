02974862788eb6288fcc388a9aed1c0af601710a
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-12-12 11:49:47 +0500

    GO

---
+++ CommandsService/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+namespace CommandsService;
+public class Startup
+{
+  public void ConfigureServices(IServiceCollection srvc)
+  {
+    srvc.AddDbContext<AppDbContext>(opt => {
+      opt.UseInMemoryDatabase("InMem");
+    });
+    srvc.AddControllers(); 
+    srvc.AddAutoMapper(
+      AppDomain.CurrentDomain.GetAssemblies()
+    );
+    srvc.AddTransient<IPlatformRepo, PlatformRepo>();
+    srvc.AddEndpointsApiExplorer(); 
+    srvc.AddSwaggerGen();
+  }
+
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+    if (env.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Platform Service");
+        c.RoutePrefix =  "swagger"; // string.Empty; // Optional: Serve Swagger UI at the app's root
+      });
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers(); // Map controller endpoints
+    });
+
+    PrepDb.PrepPopulation(app);
+  }
+}
