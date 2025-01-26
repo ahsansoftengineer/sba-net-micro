af1c96b21be6d648fde64b68470dde8365c54946
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-01-26 10:09:26 +0500

    GO

---
+++ GLOB.Infra/Common/UnitOfWorkz1.cs
@@ -0,0 +1,11 @@
+using GLOB.Apps.Common;
+using GLOB.Domain.Entity;
+
+namespace GLOB.Infra.Common;
+public partial class UnitOfWorkz
+{
+  // Test
+  private IRepoGenericz<TestEntity>? _testEntity;
+  private IRepoGenericz<TestParent>? _testParent;
+  private IRepoGenericz<TestChild>? _testChild;
+}
---
+++ GLOB.Infra/Common/UnitOfWorkz2.cs
@@ -0,0 +1,14 @@
+using GLOB.Apps.Common;
+using GLOB.Domain.Entity;
+
+namespace GLOB.Infra.Common;
+public partial class UnitOfWorkz : IUnitOfWorkz
+{
+
+    // ??= C# 9 Short-hand Syntax
+    // Hierarchy
+    public IRepoGenericz<TestEntity> TestEntitys => _testEntity ??= new RepoGenericz<TestEntity>(_context);
+    public IRepoGenericz<TestParent> TestParents => _testParent ??= new RepoGenericz<TestParent>(_context);
+    public IRepoGenericz<TestChild> TestChilds => _testChild ??= new RepoGenericz<TestChild>(_context);
+
+}
