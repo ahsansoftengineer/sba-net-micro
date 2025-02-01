9def8410b8dad791e3b49770c893857f9c4f83a8
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-02-01 16:27:21 +0500

    GO

--- GLOB.API/Controllers/Test/TestChildController.cs
+++ GLOB.API/Controllers/Test/TestChildController.cs
@@ -1,22 +1,22 @@
-using AutoMapper;
-using GLOB.API.Controllers.Base;
-using GLOB.Apps.Common;
-using GLOB.Domain.DTOs;
-using GLOB.Domain.Entity;
-using Microsoft.AspNetCore.Mvc;
+// using AutoMapper;
+// using GLOB.API.Controllers.Base;
+// using GLOB.Apps.Common;
+// using GLOB.Domain.DTOs;
+// using GLOB.Domain.Entity;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace GLOB.API.Controllers.Test;
-[Route("api/Test/[controller]")]
-[ApiController]
-public class TestChildController : BaseController<
-  TestChildController, TestChild, TestChildDtoSearch, TestChildDto, TestChildDtoCreate>
-{
-  public TestChildController(
-    ILogger<TestChildController> logger,
-    IMapper mapper,
-    IUnitOfWorkz unitOfWork) : base(logger, mapper, unitOfWork)
-  {
-    Repo = unitOfWork.TestChilds;
+// namespace GLOB.API.Controllers.Test;
+// [Route("api/Test/[controller]")]
+// [ApiController]
+// public class TestChildController : BaseController<
+//   TestChildController, TestChild, TestChildDtoSearch, TestChildDto, TestChildDtoCreate>
+// {
+//   public TestChildController(
+//     ILogger<TestChildController> logger,
+//     IMapper mapper,
+//     IUnitOfWorkz unitOfWork) : base(logger, mapper, unitOfWork)
+//   {
+//     Repo = unitOfWork.TestChilds;
 
-  }
-}
+//   }
+// }
