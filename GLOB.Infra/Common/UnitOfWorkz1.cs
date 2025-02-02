61ea97f75bfb5a53c51c7406829bd2ad3ec9ed04
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-02-02 10:13:53 +0500

    GO

--- GLOB.Infra/Common/UnitOfWorkz1.cs
+++ GLOB.Infra/Common/UnitOfWorkz1.cs
@@ -4,9 +4,11 @@ using GLOB.Domain.Entity;
 namespace GLOB.Infra.Common;
 public partial class UnitOfWorkz
 {
-  // Test
-  private IRepoGenericz<TestEntity>? _testEntity;
-  private IRepoGenericz<TestParent>? _testParent;
-  private IRepoGenericz<TestChild>? _testChild;
-  private IRepoGenericz<TestStatus>? _testStatus;
+  // Hierarchy
+  public IRepoGenericz<TestEntity> TestEntitys => _testEntity ??= new RepoGenericz<TestEntity>(_context);
+  public IRepoGenericz<TestParent> TestParents => _testParent ??= new RepoGenericz<TestParent>(_context);
+  public IRepoGenericz<TestChild> TestChilds => _testChild ??= new RepoGenericz<TestChild>(_context);
+  public IRepoGenericz<TestStatus> TestStatus => _testStatus ??= new RepoGenericz<TestStatus>(_context);
+
+
 }
