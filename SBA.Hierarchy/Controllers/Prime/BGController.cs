5733302e9793d75cde6799d4d55c0f346adfbc39
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-02-22 13:16:01 +0500

    GO

--- SBA.Hierarchy/Controllers/Prime/BGController.cs
+++ SBA.Hierarchy/Controllers/Prime/BGController.cs
@@ -1,20 +1,20 @@
-using AutoMapper;
-using GLOB.API.Controllers.Base;
-using GLOB.Domain.Entity;
-using Microsoft.AspNetCore.Mvc;
-using SBA.Hierarchy.App;
+// using AutoMapper;
+// using GLOB.API.Controllers.Base;
+// using GLOB.Domain.Entity;
+// using Microsoft.AspNetCore.Mvc;
+// using SBA.Hierarchy.App;
 
-namespace SBA.Hierarchy.Controllers.Test;
-[Route("api/Hierarchy/[controller]")]
-[ApiController]
-public class BGController : CommonController<BGController, BG>
-{
-  public BGController(
-    ILogger<BGController> logger,
-    IMapper mapper,
-    IUOW uow) : base(logger, mapper, uow)
-  {
-    Repo = uow.BGs;
+// namespace SBA.Hierarchy.Controllers.Test;
+// [Route("api/Hierarchy/[controller]")]
+// [ApiController]
+// public class BGController : CommonController<BGController, BG>
+// {
+//   public BGController(
+//     ILogger<BGController> logger,
+//     IMapper mapper,
+//     IUOW uow) : base(logger, mapper, uow)
+//   {
+//     Repo = uow.BGs;
 
-  }
-}
+//   }
+// }
