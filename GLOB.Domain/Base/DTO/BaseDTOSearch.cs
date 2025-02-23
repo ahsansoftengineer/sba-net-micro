881fad8bb654eed9b08e3ca057318dbdbff75521
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-02-23 12:17:20 +0500

    GO

--- GLOB.Domain/Base/DTO/BaseDTOSearch.cs
+++
@@ -1,9 +0,0 @@
-namespace GLOB.Domain.Base;
-public class BaseDtoSearchFull
-{
-  public int? Id { get; set; } = null;
-  public string? Title { get; set; }
-  public string? Desc { get; set; }
-  public DateTime? DateFrom { get; set; }
-  public DateTime? DateTo { get; set; }
-}
