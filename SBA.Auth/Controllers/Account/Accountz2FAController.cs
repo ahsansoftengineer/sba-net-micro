8f58bc658a0221b49b70728b49a83f6bb8cd27c3
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-04-01 10:38:25 +0500

    GO

--- SBA.Auth/Controllers/Account/Accountz2FAController.cs
+++ SBA.Auth/Controllers/Account/Accountz2FAController.cs
@@ -1,29 +1,28 @@
-// using GLOB.API.Controllers.Base;
-// using GLOB.Domain.Auth;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
+using GLOB.Domain.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace SBA.Auth.Controllers;
-// public partial class AccountController : AlphaController<AccountController>
-// {
-//   [HttpPost("[action]")]
-//   [Authorize]
-//   public async Task<IActionResult> Enable2FA()
-//   {
-//     return null;
-//   }
+namespace SBA.Auth.Controllers;
+public partial class AccountController
+{
+  [HttpPost("[action]")]
+  [Authorize]
+  public async Task<IActionResult> Enable2FA()
+  {
+    return null;
+  }
 
-//   [HttpPost("[action]")]
-//   public async Task<IActionResult> Verify2FA([FromBody] TwoFactorDto model)
+  [HttpPost("[action]")]
+  public async Task<IActionResult> Verify2FA([FromBody] TwoFactorDto model)
 
-//   {
-//     return null;
-//   }
-//   [HttpPost("[action]")]
-//   [Authorize]
-//   public async Task<IActionResult> Disable2FA()
-//   {
-//     return null;
-//   }
+  {
+    return null;
+  }
+  [HttpPost("[action]")]
+  [Authorize]
+  public async Task<IActionResult> Disable2FA()
+  {
+    return null;
+  }
 
-// }
+}
