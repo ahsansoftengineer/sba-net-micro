9b5ed7ee62381a1bb949a3acfd2286d09676551c
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-04-09 10:25:31 +0500

    HIDDEN ISSUE SOLVED

--- GLOB.Domain/_Base/Dto/DTOPagination.cs
+++ GLOB.Domain/_Base/Dto/DTOPagination.cs
@@ -22,24 +22,28 @@ public class DtoPageReq<TDtoSearch>
   public Sort? Sort { get; set; }
 }
 
-
-public class DtoPageRes<T> 
+public class DtoPage()
 {
-
-  public List<T> Records { get; set; }
-  public int Count { get; set; } = 0;
   public int PageSize { get; set; } = 10;
   public int PageNo { get; set; } = 1;
+}
+public class DtoPageRes<T> 
+{
+  public List<T> Records { get; set; }
+  public int PageNo { get; set; } = 1;
+  public int PageSize { get; set; } = 10;
+  public int Count { get; set; } = 0;
   public int TotalPages => (int)Math.Ceiling(Count / (double)PageSize);
 
   public bool HasPreviousPage => PageNo > 1 && TotalPages >= PageNo - 1;
   public bool HasNextPage => PageNo < TotalPages;
   public HttpStatusCode Status = HttpStatusCode.OK;
 
-  // public DtoPageRes()
-  // {
-  //   throw new Exception("Don't use Empty Constructor!!!!");
-  // }
+  public DtoPageRes(DtoPage data)
+  {
+    PageNo = data.PageNo;
+    PageSize = data.PageSize;
+  }
   public DtoPageRes(DtoPageRes<T> data)
   {
 
@@ -48,8 +52,4 @@ public class DtoPageRes<T>
     PageSize = data.PageSize;
     PageNo = PageNo;
   }
-
-    public DtoPageRes()
-    {
-    }
 }
