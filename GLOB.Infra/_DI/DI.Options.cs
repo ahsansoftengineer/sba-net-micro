3cd896a3e9aa46ac4be21a8f00ae37fe5db32b1e
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-05-01 07:54:47 +0500

    GO

---
+++ GLOB.Infra/_DI/DI.Options.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace GLOB.Infra.DI;
+public static partial class DI_Infra
+{
+
+  public static void Config_Options(this IServiceCollection srvc, IConfiguration config)
+  {
+  }
+}
