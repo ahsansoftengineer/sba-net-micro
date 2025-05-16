fbb2b143eb74c27932490b575be2e4f9522cb01e
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-05-16 17:54:28 +0500

    GO

--- SBA.Auth/Controllers/Account/AccountController.Social.cs
+++ SBA.Auth/Controllers/Account/AccountController.Social.cs
@@ -1,24 +1,71 @@
 using GLOB.Domain.Auth;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SBA.Auth.Controllers;
 
 public partial class AccountController
 {
-  
-  [HttpPost]
+  // Google login callback
+  [HttpGet]
+  public IActionResult LoginGoogleRedirect()
+  {
+    var redirectUrl = Url.Action("LoginGoogle", "Auth");
+    var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+    return Challenge(properties, "Google");
+  }
+  [HttpGet]
   public async Task<IActionResult> LoginGoogle([FromBody] ExternalAuthDto model)
   {
-    return null;
+    var result = await HttpContext.AuthenticateAsync("Google");
+
+    if (result.Succeeded)
+    {
+      // Use result.Principal to extract claims and authenticate the user
+      // return Ok(new { token = generatedJwtToken });
+    }
+
+    return Unauthorized();
   }
-  [HttpPost]
+
+  [HttpGet]
+  public IActionResult LoginFacebookRedirect()
+  {
+    var redirectUrl = Url.Action("LoginFacebook", "Auth");
+    var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+    return Challenge(properties, "Facebook");
+  }
+  [HttpGet]
   public async Task<IActionResult> LoginFacebook([FromBody] ExternalAuthDto model)
   {
-    return null;
+    var result = await HttpContext.AuthenticateAsync("Facebook");
+
+    if (result.Succeeded)
+    {
+      // Handle login and create JWT
+    }
+
+    return Unauthorized();
   }
-  [HttpPost]
+
+  // Apple login callback
+  [HttpGet]
+  public IActionResult LoginAppleRedirect()
+  {
+    var redirectUrl = Url.Action("LoginApple", "Auth");
+    var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+    return Challenge(properties, "Apple");
+  }
+  [HttpGet]
   public async Task<IActionResult> LoginApple([FromBody] ExternalAuthDto model)
   {
-    return null;
+    var result = await HttpContext.AuthenticateAsync("Apple");
+
+    if (result.Succeeded)
+    {
+      // Handle login and create JWT
+    }
+
+    return Unauthorized();
   }
 }
--- SBA.Auth/_DI/DI.SocialAccounts.cs
+++ SBA.Auth/_DI/DI.SocialAccounts.cs
@@ -1,14 +1,54 @@
+using Microsoft.AspNetCore.Authentication;
+
 namespace SBA.Projectz.DI;
+
 public static partial class DI_Projectz
 {
-  public static void Config_Social_Auth(this IServiceCollection srvc, IConfiguration config)
+  public static AuthenticationBuilder Config_Social_Auth(this AuthenticationBuilder srvc, IConfiguration config)
   {
-    // srvc.AddGoogle
+
+    var accounts = new SocialAccounts();
+    config.GetSection("SocialAccounts").Bind(accounts);
+    return srvc
+    .AddGoogle(opt =>
+    {
+      opt.ClientId = accounts.Google.Id;
+      opt.ClientSecret = accounts.Google.Secret;
+      opt.ReturnUrlParameter = accounts.Google.ReturnUrl;
+      opt.SaveTokens = true;
+      opt.Scope.Add("profile");
+      opt.Scope.Add("email");
+    })
+    .AddFacebook(opt =>
+    {
+      opt.AppId = accounts.Facebook.Id;
+      opt.AppSecret = accounts.Facebook.Secret;
+      opt.ReturnUrlParameter = accounts.Facebook.ReturnUrl;
+      opt.SaveTokens = true;
+      opt.Fields.Add("email");
+    })
+    .AddMicrosoftAccount(options =>
+    {
+      options.ClientId = accounts.Microsoft.Id;
+      options.ClientSecret = accounts.Microsoft.Secret;
+      options.CallbackPath = accounts.Microsoft.ReturnUrl;
+      options.SaveTokens = true;
+      options.Scope.Add("email");
+    })
+    .AddApple(options =>
+    {
+      options.ClientId = "com.your.bundle.id";
+      options.KeyId = "your-key-id";
+      options.TeamId = "your-team-id";
+      options.PrivateKey = null;
+      options.SaveTokens = true;
+    });
   }
 }
 public class SocialAccounts
 {
   public static string SectionName = "SocialAccounts";
+  public SocialAccount Microsoft { get; set; }
   public SocialAccount Google { get; set; }
   public SocialAccount Facebook { get; set; }
   public SocialAccount Apple { get; set; }
@@ -16,6 +56,9 @@ public class SocialAccounts
 }
 public class SocialAccount
 {
-  public string ClientId { get; set; }
-  public string ClientSecret { get; set; }
+  public string Id { get; set; }
+  public string Secret { get; set; }
+  public string ReturnUrl { get; set; }
+  public string AppleAuthority { get; set; } = "https://appleid.apple.com";
+  public string CallbackPath { get; set; } = "route-of-yourapp";
 }
