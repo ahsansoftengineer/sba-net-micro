8618328e41d94112d78705ca461637bee0d31f0e
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-05-21 17:21:38 +0500

    GO

--- SBA.Auth/Controllers/Profile/ProfileController.2FA.cs
+++ SBA.Auth/Controllers/Profile/ProfileController.2FA.cs
@@ -1,23 +1,22 @@
-// using GLOB.Domain.Auth;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
+using GLOB.Domain.Auth;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace SBA.Auth.Controllers;
-// public partial class ProfileController
-// {
-//   [HttpPost]
-//   public async Task<IActionResult> Enable2FA()
-//   {
-//     return null;
-//   }
-//   [HttpPost]
-//   public async Task<IActionResult> Verify2FA([FromBody] TwoFactorDto model)
-//   {
-//     return null;
-//   }
-//   [HttpPost]
-//   public async Task<IActionResult> Disable2FA()
-//   {
-//     return null;
-//   }
-// }
+namespace SBA.Auth.Controllers;
+public partial class ProfileController
+{
+  [HttpPost]
+  public async Task<IActionResult> MultiFaEnable()
+  {
+    return null;
+  }
+  [HttpPost]
+  public async Task<IActionResult> MultiFaVerify([FromBody] TwoFactorDto model)
+  {
+    return null;
+  }
+  [HttpPost]
+  public async Task<IActionResult> MultiFaDisable()
+  {
+    return null;
+  }
+}
