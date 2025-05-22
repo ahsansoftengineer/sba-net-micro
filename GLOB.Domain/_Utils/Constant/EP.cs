71f22b4b9fd632052457b640abf8e8959b5c96bb
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-05-22 10:13:58 +0500

    Enpoints

---
+++ GLOB.Domain/_Utils/Constant/EP.cs
@@ -0,0 +1,17 @@
+namespace GLOB.Domain.Contants;
+public static class EP
+{
+    public const string Create = "create";
+    public const string Update = "update";
+    public const string Delete = "delete";
+    public const string Status = "status";
+    public const string Get = "get";
+    public const string Gets = "gets";
+    public const string GetsPaginate = "gets-paginate";
+    public const string GetsPaginateOptions = "gets-paginate-options";
+    public const string GetsLookup = "gets-lookup";
+    public const string GetsByIds = "gets-by-ids";
+    public const string GetsByIdsLookup = "gets-by-ids-lookup";
+    public const string GetsCSV = "gets-csv";
+
+}
