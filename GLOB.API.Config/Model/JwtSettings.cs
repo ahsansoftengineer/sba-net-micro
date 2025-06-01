0c82c9f64157bad40777f2fdefea2338ded66433
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-06-01 16:03:56 +0500

    REMOVED USELESS CODE

--- GLOB.API.Config/Model/JwtSettings.cs
+++
@@ -1,4 +0,0 @@
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-
-namespace GLOB.API.Config.Model;
--- __Backup/CommandsService/Models/Platform.cs
+++
@@ -1,8 +0,0 @@
-namespace CommandsService.Models;
-public class Platform
-{
-  public int Id { get; set; }
-  public int ExternalID { get; set; }
-  public string Name { get; set; }
-  public ICollection<Command> Commands { get; set; }
-}
