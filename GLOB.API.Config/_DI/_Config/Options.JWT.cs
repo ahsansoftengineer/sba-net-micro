931136c70fd8404618a0101f32de6728df3ed32f
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-06-01 17:00:02 +0500

    GO

--- GLOB.API.Config/_DI/_Config/Options.JWT.cs
+++ GLOB.API.Config/_DI/_Config/Options.JWT.cs
@@ -7,24 +7,24 @@ namespace GLOB.API.Config.Configz;
 public class JwtSettings
 {
 
-  public static string SectionName = "JwtSettings";
-  public string SecretKey { get; set; } //"YourSuperStrongSecretKey_ReplaceThis"
-  public string Issuer { get; set; } // "https://localhost:1102/"
-  public string Audience { get; set; } // "https://localhost:1102/"
-  public int AccessTokenExpiryMinutes { get; set; } //60 -> 15
-  public int AccessTokenExpiryHour { get; set; } //1
-  public int RefreshTokenExpiryDays { get; set; } //7
-  public bool ValidateIssuer { get; set; }// = true;
-  public bool ValidateAudience { get; set; }// = true;
-  public bool ValidateLifetime { get; set; }// = true;
-  public bool ValidateIssuerSigningKey { get; set; }// = true;
+  public static string? SectionName = "JwtSettings";
+  public string? SecretKey { get; set; } //"YourSuperStrongSecretKey_ReplaceThis"
+  public string? Issuer { get; set; } // "https://localhost:1102/"
+  public string? Audience { get; set; } // "https://localhost:1102/"
+  public int? AccessTokenExpiryMinutes { get; set; } //60 -> 15
+  public int? AccessTokenExpiryHour { get; set; } //1
+  public int? RefreshTokenExpiryDays { get; set; } //7
+  public bool? ValidateIssuer { get; set; }// = true;
+  public bool? ValidateAudience { get; set; }// = true;
+  public bool? ValidateLifetime { get; set; }// = true;
+  public bool? ValidateIssuerSigningKey { get; set; }// = true;
 
   // Cookie
-  public static string Scheme = "AuthorizationCookieScheme";
-  public string LoginPath { get; set; }
-  public string CookieName { get; set; }
-  public string AccessDeniedPath { get; set; }
-  public bool SlidingExpiration { get; set; }// = true;
+  public static string? Scheme = "AuthorizationCookieScheme";
+  public string? LoginPath { get; set; }
+  public string? CookieName { get; set; }
+  public string? AccessDeniedPath { get; set; }
+  public bool? SlidingExpiration { get; set; }// = true;
 
 
   public SymmetricSecurityKey GetSymmetricSecurityKey()
--- GLOB.API.Config/_DI/_Config/Options.Srvc.cs
+++ GLOB.API.Config/_DI/_Config/Options.Srvc.cs
@@ -2,21 +2,21 @@ namespace GLOB.API.Config.Configz;
 
 public class SrvcHttp
 {
-    public string Gateway { get; set; }
-    public string Auth { get; set; }
-    public string Hierarchy { get; set; }
+    public string? Gateway { get; set; }
+    public string? Auth { get; set; }
+    public string? Hierarchy { get; set; }
 }
 
 public class SrvcGRPC
 {
-    public string Gateway { get; set; }
-    public string Auth { get; set; }
-    public string Hierarchy { get; set; }
+    public string? Gateway { get; set; }
+    public string? Auth { get; set; }
+    public string? Hierarchy { get; set; }
 }
 
 public class SrvcRabbitMQ
 {
-    public string Gateway { get; set; }
-    public string Auth { get; set; }
-    public string Hierarchy { get; set; }
+    public string? Gateway { get; set; }
+    public string? Auth { get; set; }
+    public string? Hierarchy { get; set; }
 }
