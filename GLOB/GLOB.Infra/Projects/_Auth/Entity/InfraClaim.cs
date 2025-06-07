30fa9b38d661935b67c8a62151c79bbc5321ed9c
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2025-06-07 17:21:41 +0500

    GO

--- GLOB/GLOB.Infra/Projects/_Auth/Entity/InfraClaim.cs
+++ GLOB/GLOB.Infra/Projects/_Auth/Entity/InfraClaim.cs
@@ -3,9 +3,8 @@ using GLOB.Domain.Auth.Enumz;
 namespace GLOB.Domain.Auth;
 public class UserPermission
 {
-
-    public int Id { get; set; }
-    public string RoleId { get; set; }
-    public string Entity { get; set; } // Example: "Employee", "Payroll"
-    public Permission Permissions { get; set; }
+  public int Id { get; set; }
+  public string RoleId { get; set; }
+  public string Entity { get; set; } // Example: "Employee", "Payroll"
+  public Permission Permissions { get; set; }
 }
