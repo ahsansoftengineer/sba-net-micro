539df31321eca40222baf47fa15d541936a8d3cd
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-07-04 17:08:36 +0500

    GO

--- GLOB/GLOB.API.Config/_DI/DI.Serilog.cs
+++ GLOB/GLOB.API.Config/_DI/DI.Serilog.cs
@@ -7,9 +7,10 @@ public static partial class DI_API_Config
 {
   public static void Reg_API_Config_Serilog()
   {
+    // Verbose,Debug,Information,Warning,Error,Fatal
     Log.Logger = new LoggerConfiguration()
       .MinimumLevel.Debug() // or Information in production
-      .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+      .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
       .Enrich.FromLogContext()
       .WriteTo.Console(
           outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
