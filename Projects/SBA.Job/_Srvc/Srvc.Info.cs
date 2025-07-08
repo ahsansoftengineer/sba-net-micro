0bd4b84d1512f10a8b508b2b314cc326278cb2d6
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-07-08 10:40:03 +0500

    GO

---
+++ Projects/SBA.Job/_Srvc/Srvc.Info.cs
@@ -0,0 +1,19 @@
+
+namespace SBA.Projectz.Srvc;
+
+public class SrvcInfo
+{
+  public void SendEmail()
+  {
+
+  }
+  public void SendNotification()
+  {
+
+  }
+  public void SendSMS()
+  {
+    
+  }
+  
+}
---
+++ Projects/SBA.Job/_Srvc/Srvc.ProjectzLookup.cs
@@ -0,0 +1,28 @@
+
+namespace SBA.Projectz.Srvc;
+
+public class SrvcProjectzLookup
+{
+  public void GenerateMerchandise()
+  {
+
+  }
+  public void BulkCreate()
+  {
+
+  }
+
+  public void BulkUpdate()
+  {
+
+  }
+  public void SyncData()
+  {
+
+  }
+  public void UpdateDatabase()
+  {
+
+  }
+  
+}
