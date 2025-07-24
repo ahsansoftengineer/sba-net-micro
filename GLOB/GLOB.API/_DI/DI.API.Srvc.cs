3694459b1d889d6587dad1fefcd97fad99e1abbb
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-07-24 15:13:16 +0500

    GO

--- GLOB/GLOB.API/_DI/DI.API.Srvc.cs
+++ GLOB/GLOB.API/_DI/DI.API.Srvc.cs
@@ -4,7 +4,8 @@ public static partial class DI_API
   public static void Add_API_Default_Srvc(this IServiceCollection srvc, IConfiguration config)
   {
     //srvc.Add_API_Config_Localization(config);
-    // srvc.Add_API_Config_Options(config);
+    srvc.Add_API_Config_Options(config);
+    srvc.AddSingleton<HttpClient>();
     srvc.Add_API_Controller_Srvc_Extend(config);
 
     // srvc.Add_API_Config_Authentication_JWT(config);
@@ -13,14 +14,9 @@ public static partial class DI_API
     srvc.Add_API_Config_Swagger(config);
     srvc.Add_API_Config_Cors();
     // srvc.AddAutoMapper(typeof(API_Base_Mapper));
-  }
 
-  public static void Add_API_Default_Srvc2(this IServiceCollection srvc)
-  {
-    srvc.Add_API_Config_Versioning();
-    srvc.Add_API_Config_HttpCacheHeaders();
-    srvc.Add_API_Config_RateLimiting();
-    // srvc.Add_API_Config_StaticFiles();
+    // srvc.Add_API_Config_Versioning();
+    // srvc.Add_API_Config_HttpCacheHeaders();
+    // srvc.Add_API_Config_RateLimiting();
   }
-
 }
